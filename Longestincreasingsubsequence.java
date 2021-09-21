26da196418ccdf035c896bef07ca3d3e647a8b4a
amitk95 <akghiyal1995@gmail.com>
2021-09-21 22:53:32 +0530

    1st commit LIS

---
+++ Longestincreasingsubsequence.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+      
+      Scanner sc= new Scanner(System.in);
+      int a= sc.nextInt();
+      String b=sc.nextLine();
+      a=a*2;
+      System.out.println(a);
+      System.out.println(b);
+
+    }
+}
